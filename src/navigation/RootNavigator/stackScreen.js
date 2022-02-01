be636ef494d19698f5adee7086c9d1ad72e493bd
oğulcan erişti <ogulcaneristi123@gmail.com>
2022-02-01 13:02:13 +0300

    update stackscreen

--- src/navigation/RootNavigator/stackScreen.js
+++ src/navigation/RootNavigator/stackScreen.js
@@ -18,13 +18,43 @@ const options = {
 
 const StackScreen = () => {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Movies">
       <Stack.Screen options={() => options} name="Movies" component={RootNavigator} />
       <Stack.Screen options={() => options} name="Details" component={Details} />
       <Stack.Screen options={() => options} name="Favorites" component={Favorites} />
       <Stack.Screen options={() => options} name="Profile" component={Profile} />
-      <Stack.Screen options={() => options} name="Login" component={Login} />
-      <Stack.Screen options={() => options} name="SignUp" component={Signup} />
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: '#0f0f1a',
+          },
+        }}
+        name="Login"
+        component={Login}
+      />
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerBackTitleVisible: false,
+          headerTitleStyle: {
+            color: '#fff',
+          },
+          headerTitle: 'Sign Up',
+          headerStyle: {
+            backgroundColor: '#0f0f1a',
+          },
+          cardStyleInterpolator: ({ current: { progress } }) => {
+            return {
+              cardStyle: {
+                opacity: progress,
+              },
+            };
+          },
+        }}
+        name="SignUp"
+        component={Signup}
+      />
     </Stack.Navigator>
   );
 };
